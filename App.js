6a24d73b164a7473fc95631f59ea82e8ed5ac7ab
Yusril Izza <zaulsr14@gmail.com>
2020-03-26 22:28:23 +0700

    init navigation

--- App.js
+++ App.js
@@ -1,114 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import 'react-native-gesture-handler';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {useState} from 'react';
+// import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import HomeScreen from './src/screens/home';
+import DashboardSceeen from './src/screens/dashboard';
+import AccountScreen from './src/screens/account';
+import LoginScreen from './src/screens/public/login';
 
-const App: () => React$Node = () => {
+function HomeStackScreen() {
+  const HomeStack = createStackNavigator();
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+    </HomeStack.Navigator>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+function DashboardStackScreen() {
+  const DashboardStack = createStackNavigator();
+  return (
+    <DashboardStack.Navigator>
+      <DashboardStack.Screen name="Dashboard" component={DashboardSceeen} />
+    </DashboardStack.Navigator>
+  );
+}
+
+function AccountStackScreen() {
+  const AccountStack = createStackNavigator();
+  return (
+    <AccountStack.Navigator>
+      <AccountStack.Screen name="Dashboard" component={AccountScreen} />
+    </AccountStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+const PublicStack = createStackNavigator();
+
+function App() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <NavigationContainer>
+      {isLogin ? (
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeStackScreen} />
+          <Tab.Screen name="Dashboard" component={DashboardStackScreen} />
+          <Tab.Screen name="Account" component={AccountStackScreen} />
+        </Tab.Navigator>
+      ) : (
+        <PublicStack.Navigator initialRouteName="Login">
+          <PublicStack.Screen name="Login" component={LoginScreen} />
+        </PublicStack.Navigator>
+      )}
+    </NavigationContainer>
+  );
+}
 
 export default App;
---
+++ src/screens/public/login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Login dlu anjay</Text>
+      <Button title="Go to Details" />
+    </View>
+  );
+}
+
+export default HomeScreen;
